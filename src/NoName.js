62716369d745a1a19f6c6d0e53793f30c9658c79
jeonghyun <chlwjdgus822@gmail.com>
2023-10-30 15:58:07 +0900

    1. React 초기 설정 완료 및 Component 개념 확인

--- src/NoName.js
+++ src/NoName.js
@@ -1,11 +1,10 @@
-import React from "react";
-
 class NoName extends React.Component {
   render() {
-    return {
-        <div className='wrap'>
-            <h1>하이하이</h1>
-        </div>
-    };
+    return (
+      <div className='wrap'>
+        <h1>아히아히</h1>
+      </div>
+    );
   }
 }
+export default NoName;
